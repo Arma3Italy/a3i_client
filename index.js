05e88181fbd1fa3b29097f2ae81a19cbc5514618
Kaj Oskar Rusilowski <xedom08@gmail.com>
2018-06-17 04:06:05 +0200

    updated serverListUpdater

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 
 const web = require('./src/app.js')();
 
-// const serverUpdater = require('./api/serverList/serverBrowser.js').serverListUpdater;
-// setInterval(serverUpdater, 2000);
+const serverUpdater = require('./api/serverList/serverBrowser.js').serverListUpdater;
+setInterval(serverUpdater, 2000);
 
 
 
