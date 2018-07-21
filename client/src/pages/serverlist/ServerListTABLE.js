188810c9c800e56c7e5b83b6b66623cc08ea4974
Kaj Oskar Rusilowski <xedom08@gmail.com>
2018-07-21 03:48:40 +0200

    [ServerListTABLE] added badges

--- client/src/pages/serverlist/ServerListTABLE.js
+++ client/src/pages/serverlist/ServerListTABLE.js
@@ -18,7 +18,7 @@ class ServerListTABLE extends Component {
         const sl = this.state.servers.map(i => (
             <div className="ST_body row bg-light text-dark p-2 border-bottom" key={String(i.addr)}>
                 <div className="col-lg-1 d-none d-lg-block text-center">{i.rank[0].like.length}</div>
-                <div className="col-6 col-lg-4 text-wrap">{this.serverStatusChecker(i)} <a href={`/server/${i.addr}`}>{i.name}</a></div>
+                <div className="col-6 col-lg-4 text-wrap">{this.serverStatusChecker(i)} <a href={`/server/${i.addr}`}><span className="badge badge-primary">{i.gametype === undefined ? 'NONE' : i.gametype}</span> {i.name}</a></div>
                 <div className="col-6 col-lg-3"><a className="text-warning"><i className="fas fa-copy"></i></a> {i.addr.split(':')[0]}:{i.gameport}</div>
                 <div className="col-lg-2 d-none d-lg-block text-center">{i.map}</div>
                 <div className="col-lg-1 d-none d-lg-block text-center">{i.players}/{i.max_players}</div>
