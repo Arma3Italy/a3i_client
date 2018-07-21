3b372d8f854e9a82e2d277ed23fac97137731e34
Kaj Oskar Rusilowski <xedom08@gmail.com>
2018-07-21 17:07:23 +0200

    [ServerListTABLE] fixed grid spaceing

--- client/src/pages/serverlist/ServerListTABLE.js
+++ client/src/pages/serverlist/ServerListTABLE.js
@@ -17,12 +17,53 @@ class ServerListTABLE extends Component {
     listItem() {
         const sl = this.state.servers.map(i => (
             <div className="ST_body row bg-light text-dark p-2 border-bottom" key={String(i.addr)}>
-                <div className="col-lg-1 d-none d-lg-block text-center">{i.rank[0].like.length}</div>
-                <div className="col-6 col-lg-4 text-wrap">{this.serverStatusChecker(i)} <a href={`/server/${i.addr}`}><span className="badge badge-primary">{i.gametype === undefined ? 'NONE' : i.gametype}</span> {i.name}</a></div>
-                <div className="col-6 col-lg-3"><a className="text-warning"><i className="fas fa-copy"></i></a> {i.addr.split(':')[0]}:{i.gameport}</div>
-                <div className="col-lg-2 d-none d-lg-block text-center">{i.map}</div>
-                <div className="col-lg-1 d-none d-lg-block text-center">{i.players}/{i.max_players}</div>
-                <div className="col-lg-1 d-none d-lg-block text-center"><a rel="noopener noreferrer" href={`steam://run/107410//-connect=${i.addr.split(':')[0]}:${i.gameport}`}><i className="fas fa-sign-in-alt"></i></a></div>
+                <div className="col-12 col-lg-6">
+                    <div className="row">
+                        {/* RANK */}<div className="col-lg-1 d-none d-lg-block text-center">
+                                        {i.rank[0].like.length}
+                                    </div>
+                        {/* NAME */}<div className="col-12 col-lg-11 text-wrap">
+                                        {this.serverStatusChecker(i)} 
+                                        <a href={`/server/${i.addr}`} title={i.name}>
+                                            {' '}<span className="badge badge-primary">
+                                                {i.gametype === undefined ? 'NONE' : i.gametype}
+                                            </span> 
+                                            {i.name.length > 30 ? ' '+i.name.substring(0,35)+' ...' : ' '+i.name}
+                                        </a>
+                                    </div>
+                    </div>
+                </div>
+
+                <div className="col-12 col-lg-6">
+                    <div className="row">
+                        <div className="col-12 col-lg-9">
+                            <div className="row">
+                                {/* IP */}  <div className="col-12 col-lg-7">
+                                                <a className="text-warning">
+                                                    <i className="fas fa-copy"></i>
+                                                </a>
+                                                {' '+i.addr.split(':')[0]}:{i.gameport}
+                                            </div>
+                                {/* MAPPA */}<div className="col-lg-5 d-none d-lg-block text-center">
+                                                {i.map}
+                                            </div>
+                            </div>
+                        </div>
+
+                        <div className="col-lg-3">
+                            <div className="row">
+                                {/* PLAYERS */} <div className="col-lg-7 d-none d-lg-block text-center">
+                                                    {i.players}/{i.max_players}
+                                                </div>
+                                {/* RANK */}<div className="col-lg-5 d-none d-lg-block text-center">
+                                                <a rel="noopener noreferrer" href={`steam://run/107410//-connect=${i.addr.split(':')[0]}:${i.gameport}`}>
+                                                    <i className="fas fa-sign-in-alt"></i>
+                                                </a>
+                                            </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         ));
 
@@ -37,12 +78,31 @@ class ServerListTABLE extends Component {
         return (
             <div className="serverTABLE">
                 <div className="ST_head row bg-dark p-2">
-                    <div className="col-lg-1 d-none d-lg-block text-center">Rank</div>
-                    <div className="col-6 col-lg-4 text-wrap">Nome</div>
-                    <div className="col-6 col-lg-3">IP</div>
-                    <div className="col-lg-2 d-none d-lg-block text-center">Mappa</div>
-                    <div className="col-lg-1 d-none d-lg-block text-center">Players</div>
-                    <div className="col-lg-1 d-none d-lg-block text-center">Entra</div>
+                    <div className="col-8 col-lg-6">
+                        <div className="row">
+                            <div className="col-lg-1 d-none d-lg-block text-center">Rank</div>
+                            <div className="col-12 col-lg-11 text-wrap">Nome</div>
+                        </div>
+                    </div>
+                    
+
+                    <div className="col-4 col-lg-6">
+                        <div className="row">
+                            <div className="col-12 col-lg-9">
+                                <div className="row">
+                                    <div className="col-12 col-lg-7">IP</div>
+                                    <div className="col-lg-5 d-none d-lg-block text-center">Mappa</div>
+                                </div>
+                            </div>
+                            
+                            <div className="col-lg-3">
+                                <div className="row">
+                                    <div className="col-lg-7 d-none d-lg-block text-center">Players</div>
+                                    <div className="col-lg-5 d-none d-lg-block text-center"><i className="fas fa-sign-in-alt"></i></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
                 { this.listItem() }
