313e8d8d1dc1907d99d01f313aee8ec51c73a8b9
Kaj Oskar Rusilowski <xedom08@gmail.com>
2018-07-28 00:10:54 +0200

    [app] removed steam login and login route

--- src/app.js
+++ src/app.js
@@ -2,12 +2,7 @@ module.exports = function () {
     const express = require('express');
     const app = express();
     const port = 8888 || process.env.PORT;
-    const bodyParser = require('body-parser');
     const keys = require('../cfg/cfg');
-
-    // Steam login
-    const steamLogin = require('steam-login');
-    app.use(require('express-session')({ resave: false, saveUninitialized: false, secret: keys.sessionKey }));
 
     // Connecction to DB
     const mongoose = require('mongoose');
@@ -17,10 +12,6 @@ module.exports = function () {
     app.use('/', express.static('static'));
     app.set('view engine', 'pug');
     app.set('views', './views');
-
-    // Settings BodyParser
-    app.use(bodyParser.urlencoded({ extended: false}));
-    app.use(bodyParser.json());
 
     // Main Rounts
     app.get('/', (req, res) => {
@@ -33,7 +24,6 @@ module.exports = function () {
 
     // Routs
     app.use('/api/serverList', require('../api/serverList'));
-    app.use('/api/login', require('../api/login').route);
 
     // Start server
     app.listen(port, () => console.log(`-> Server started on port: ${port}`));
