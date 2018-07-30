5e6476744d2bcffbe0e51ba12aa8628838df8ef7
Kaj Oskar Rusilowski <xedom08@gmail.com>
2018-07-30 15:19:21 +0200

    [Profile] added user date requesto to server

--- client/src/pages/profile/Profile.js
+++ client/src/pages/profile/Profile.js
@@ -11,32 +11,40 @@ class Profile extends Component {
             token: this.props.location.search.split('?token=')[1],
             user: {
                 name: 'Debian',
-                img: 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/e6/e6759921dc6e1c4622731900793c1e625067301a_full.jpg'
+                avatar: 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/e6/e6759921dc6e1c4622731900793c1e625067301a_full.jpg'
             }
         };
     };
 
     componentDidMount() {
         // TODO: create post request to backend with token to get user data
-        // fetch(`${cfg.serverApp.port}${cfg.serverApp.host}:${cfg.serverApp.port}/api/login/user`,{
-        //     method: 'post',
-        //     body: this.state.token
-        // })
-        //     .then(data => data.json())
-        //     .then(console.log);
+        fetch(`${cfg.serverApp.protocol}${cfg.serverApp.host}:${cfg.serverApp.port}/api/login/user`, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                token: this.state.token
+            })
+        })
+            .then(data => data.json())
+            .then(user => this.setState({
+                user: user._doc
+            }));
     };
 
     render() {
-        console.log(this.state.token);
+        console.log(this.state)
         return (
             <div className="Profile">
                 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossOrigin="anonymous" />
                 <Header />
                 <main className="container-fluid my-3">
-                    <h2 className="text-center">{this.state.user.name}</h2>
                     <div className="row">
                         <div>
-                            <img className="img-fluid m-0 p-0" alt="Profile_Avatar" src={this.state.user.img} />
+                            <img className="img-fluid m-0 p-0" alt="Profile_Avatar" src={this.state.user.avatar} />
+                            <h2 className="text-center">{this.state.user.name}</h2>
                         </div>
 
                     </div>
