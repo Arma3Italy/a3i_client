4db11bfd6ffaa7777fd66c70d336b7385e4fd74b
Kaj Oskar Rusilowski <xedom08@gmail.com>
2018-07-31 22:20:46 +0200

    [ServerListTABLE] added undefined / empty array of servers check

--- client/src/pages/serverlist/ServerListTABLE.js
+++ client/src/pages/serverlist/ServerListTABLE.js
@@ -20,55 +20,66 @@ class ServerListTABLE extends Component {
     };
 
     listItem() {
-        const sl = this.state.servers.map(i => (
-            <div className="ST_body row bg-light text-dark p-2 border-bottom" key={String(i.addr)}>
-                <div className="col-12 col-lg-6">
-                    <div className="row">
-                        {/* RANK */}<div className="col-lg-1 d-none d-lg-block text-right">
-                                        {i.rank[0].like.length}
-                                    </div>
-                        {/* NAME */}<div className="col-12 col-lg-11 text-wrap">
-                                        {this.serverStatusChecker(i)} 
-                                        <a href={`/server/${i.addr.split(':')[0]}:${i.gameport}`} title={i.name}>
-                                            {' '}<span className="badge badge-primary">
-                                                {i.gametype === undefined ? 'NONE' : i.gametype}
-                                            </span> 
-                                            { window.innerWidth < 400 ? (i.name.length > 20 ? ' '+i.name.substring(0,25)+' ...' : ' '+i.name) : (i.name.length > 30 ? ' '+i.name.substring(0,35)+' ...' : ' '+i.name) }
-                                        </a>
-                                    </div>
+        let sl;
+        if (this.state.servers === undefined || this.state.servers.length === 0) {
+            sl = (
+                <div className="ST_body row bg-light text-dark p-2 border-bottom">
+                    <div className="col-12">
+                        <p>Nessun server prova a ricaricare la pagina . . .</p>
                     </div>
                 </div>
-
-                <div className="col-12 col-lg-6">
-                    <div className="row">
-                        <div className="col-12 col-lg-9">
-                            <div className="row">
-                                {/* IP */}  <div className="col-12 col-lg-7">
-                                                <i className="fas fa-copy text-warning" onClick={() => {this.copyToClip(`${' '+i.addr.split(':')[0]}:${i.gameport}`)}}></i>
-                                                {' '+i.addr.split(':')[0]}:{i.gameport}
-                                            </div>
-                                {/* MAPPA */}<div className="col-lg-5 d-none d-lg-block text-center">
-                                                {i.map}
-                                            </div>
-                            </div>
+            );
+        } else {
+            sl = this.state.servers.map(i => (
+                <div className="ST_body row bg-light text-dark p-2 border-bottom" key={String(i.addr)}>
+                    <div className="col-12 col-lg-6">
+                        <div className="row">
+                            {/* RANK */}<div className="col-lg-1 d-none d-lg-block text-right">
+                                            {i.rank[0].like.length}
+                                        </div>
+                            {/* NAME */}<div className="col-12 col-lg-11 text-wrap">
+                                            {this.serverStatusChecker(i)} 
+                                            <a href={`/server/${i.addr.split(':')[0]}:${i.gameport}`} title={i.name}>
+                                                {' '}<span className="badge badge-primary">
+                                                    {i.gametype === undefined ? 'NONE' : i.gametype}
+                                                </span> 
+                                                { window.innerWidth < 400 ? (i.name.length > 20 ? ' '+i.name.substring(0,25)+' ...' : ' '+i.name) : (i.name.length > 30 ? ' '+i.name.substring(0,35)+' ...' : ' '+i.name) }
+                                            </a>
+                                        </div>
                         </div>
-
-                        <div className="col-lg-3">
-                            <div className="row">
-                                {/* PLAYERS */} <div className="col-lg-7 d-none d-lg-block text-center">
-                                                    {i.players}/{i.max_players}
+                    </div>
+    
+                    <div className="col-12 col-lg-6">
+                        <div className="row">
+                            <div className="col-12 col-lg-9">
+                                <div className="row">
+                                    {/* IP */}  <div className="col-12 col-lg-7">
+                                                    <i className="fas fa-copy text-warning" onClick={() => {this.copyToClip(`${' '+i.addr.split(':')[0]}:${i.gameport}`)}}></i>
+                                                    {' '+i.addr.split(':')[0]}:{i.gameport}
                                                 </div>
-                                {/* RANK */}<div className="col-lg-5 d-none d-lg-block text-center">
-                                                <a rel="noopener noreferrer" href={`steam://run/107410//-connect=${i.addr.split(':')[0]}:${i.gameport}`}>
-                                                    <i className="fas fa-sign-in-alt"></i>
-                                                </a>
-                                            </div>
+                                    {/* MAPPA */}<div className="col-lg-5 d-none d-lg-block text-center">
+                                                    {i.map}
+                                                </div>
+                                </div>
+                            </div>
+    
+                            <div className="col-lg-3">
+                                <div className="row">
+                                    {/* PLAYERS */} <div className="col-lg-7 d-none d-lg-block text-center">
+                                                        {i.players}/{i.max_players}
+                                                    </div>
+                                    {/* RANK */}<div className="col-lg-5 d-none d-lg-block text-center">
+                                                    <a rel="noopener noreferrer" href={`steam://run/107410//-connect=${i.addr.split(':')[0]}:${i.gameport}`}>
+                                                        <i className="fas fa-sign-in-alt"></i>
+                                                    </a>
+                                                </div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        ));
+            ));
+        };
 
         return sl;
     };
@@ -79,7 +90,7 @@ class ServerListTABLE extends Component {
 
     render() {
         return (
-            <div className="serverTABLE">
+            <div className="serverTABLE text-light">
                 <div className="ST_head row bg-dark p-2">
                     <div className="col-8 col-lg-6">
                         <div className="row">
