ae6aed31e80c178732255166d12974a3c6206ef8
Kaj Oskar Rusilowski <xedom08@gmail.com>
2018-07-31 22:23:06 +0200

    [Profile] modified img, added steamid and owned server

--- client/src/pages/profile/Profile.js
+++ client/src/pages/profile/Profile.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from '../Header';
 import Footer from '../Footer';
+import ServerTABLE from '../serverlist/ServerListTABLE';
 import cfg from '../../cfg/cfg';
 
 class Profile extends Component {
@@ -39,14 +40,16 @@ class Profile extends Component {
             <div className="Profile">
                 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossOrigin="anonymous" />
                 <Header />
-                <main className="container-fluid my-3">
-                    <div className="row">
-                        <div>
-                            <img className="img-fluid m-0 p-0" alt="Profile_Avatar" src={this.state.user.avatar} />
-                            <h2 className="text-center">{this.state.user.name}</h2>
+                <main className="container-fluid">
+                    <div className="bg-warning row my-2">
+                        <img className="img-fluid" alt="Profile_Avatar" src={this.state.user.avatar} />
+                        <div className="p-2">
+                            <h2>{this.state.user.name}</h2>
+                            <h4>SteamID: {this.state.user.steamid}</h4>
                         </div>
-
                     </div>
+
+                    <ServerTABLE servers={{ serverList: this.state.user.servers }} />
                 </main>
                 <Footer />
             </div>
