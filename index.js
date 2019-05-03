88db182fd3a350cf713c43c5b234f76f5f266684
xedom <xedom08@gmail.com>
2019-05-03 21:04:56 +0200

    rework

--- index.js
+++
@@ -1,9 +0,0 @@
-// const serverUpdater = require('./src/serverBrowser.js').serverListUpdater;
-// const serverUpdateTime = require('./cfg/cfg').steam.serverListCFG.updateTime * (1000*60);
-
-// serverUpdater();
-// setInterval(serverUpdater, serverUpdateTime);
-
-setTimeout(() => {
-    const web = require('./src/app.js')();
-}, 4000);
