9be8ae96d018234b758549923ec96ee92ff8e149
Kaj Oskar Rusilowski <xedom08@gmail.com>
2019-05-04 02:08:42 +0200

    classes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import './Home.scss';
 
+
 function Home() {
   return (
     <div className="Home">
-      <div className="cont">
+      <div className="Home-hover">
+      </div>
+      <div className="Home-content">
         <div className="head">
           <h1>Arma 3 Italy</h1>
           <hr/>
           <p>Aiutiamoci a vicenda</p>
         </div>
         <div className="social">
-          <a href="#1">Discord</a>
-          <a href="#2">Servers</a>
-          <a href="#3">Workshop</a>
-          <a href="#4">Login</a>
+          <a className="btn btn-dark btn-box round" href="#1">Discord</a>
+          <a className="btn btn-dark btn-box round" href="#2">Servers</a>
+          <a className="btn btn-dark btn-box round" href="#3">Workshop</a>
+          <a className="btn btn-dark btn-box round" href="#4">Login</a>
         </div>
       </div>
     </div>
