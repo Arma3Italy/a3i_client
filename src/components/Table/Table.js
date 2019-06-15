74ce410ed0c66ac76c867467cd6aa19318ceacb2
xedom <xedom08@gmail.com>
2019-06-15 02:43:59 +0200

    table

---
+++ src/components/Table/Table.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Table.scss';
+
+const Row = () => (
+  <div className="Row">
+    <div>1</div>
+    <div>2</div>
+    <div>3</div>
+  </div>
+);
+
+const Table = () => (
+  <div className="Table">
+    <div className="head">
+      <h2>fsadfasf</h2>
+      <Row />
+    </div>
+  </div>
+);
+
+export default Table;
--- src/pages/Server/Server.js
+++ src/pages/Server/Server.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Server.scss';
 import Navbar from './../../components/Navbar/Navbar.js';
+import Table from './../../components/Table/Table.js';
 
 const Servers = () => (
   <div className="Servers">
     <Navbar />
+    <Table />
   </div>
 );
 
