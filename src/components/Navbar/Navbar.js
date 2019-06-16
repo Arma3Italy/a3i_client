2b1393b1b2b726ed43705a4873d124d414079445
Kaj Oskar Rusilowski <xedom08@gmail.com>
2019-06-16 21:31:46 +0200

    updated links

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,9 +5,9 @@ const Navbar = () => (
   <div className="Navbar">
     <h2 id="logo"><a href="#arma3italy">A3I</a></h2>
     <ul id="links">
-        <li><a href="#Home">Home</a></li>
-        <li><a href="#Discord">Discord</a></li>
-        <li><a href="#Steam">Steam</a></li>
+        <li><a href="/">Home</a></li>
+        <li><a target="_black" href="https://discord.gg/R4d5HBB">Discord</a></li>
+        <li><a target="_black" href="https://steamcommunity.com/groups/A3ITA">Steam</a></li>
     </ul>
 
     <a id="login" href="#login">Login</a>
