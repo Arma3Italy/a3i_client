a4e556fa9e0a75dae31d206261f5ff996dea12c5
Kaj Oskar Rusilowski <xedom08@gmail.com>
2019-06-16 02:46:13 +0200

    table

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import './Table.scss';
 
-const Row = () => (
-  <div className="Row">
-    <div>1</div>
-    <div>2</div>
-    <div>3</div>
-  </div>
-);
+const Row = (props) => {
+  const col = props.obj.map(title => (<div className="item">{title}</div>));
+  return (<div className={props.type}>{col}</div>);
+};
 
-const Table = () => (
-  <div className="Table">
-    <div className="head">
-      <h2>fsadfasf</h2>
-      <Row />
-    </div>
-  </div>
-);
+const Table = (props) => {
+
+  return (<div className="Table">
+    <Row key="99" type="head" obj={props.header} />
+    {props.body.map(server => (<Row key={server.id} type="body" obj={[server.rank, server.name, server.players, server.modalita, server.join]} />))}
+  </div>);
+};
 
 export default Table;
