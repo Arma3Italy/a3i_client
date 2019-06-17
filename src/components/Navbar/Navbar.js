cf8789bdb3c635fe44794603bea3e69944cc1db3
Kaj Oskar Rusilowski <xedom08@gmail.com>
2019-06-17 14:59:48 +0200

    new navbar links

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,9 +4,9 @@ import logo from '../../assets/wallpapers/logo.png';
 
 const Navbar = () => (
   <div className="Navbar">
-    <h2 id="logo"><a href="#arma3italy"> <img src={logo} alt=""/> </a></h2>
+    <h2 id="logo"><a href="/"> <img src={logo} alt=""/> </a></h2>
     <ul id="links">
-        <li><a href="/">Home</a></li>
+        <li><a href="/server">Servers</a></li>
         <li><a target="_black" href="https://discord.gg/R4d5HBB">Discord</a></li>
         <li><a target="_black" href="https://steamcommunity.com/groups/A3ITA">Steam</a></li>
     </ul>
