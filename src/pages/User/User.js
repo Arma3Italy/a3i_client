e200ccf5c74760c0703442ea4417ae05a2ee1a29
Kaj Oskar Rusilowski <xedom08@gmail.com>
2019-06-17 22:45:18 +0200

    placeholder

--- src/pages/User/User.js
+++ src/pages/User/User.js
@@ -9,7 +9,7 @@ const User = () => (
     <div className="User-hover"></div>
     <Navbar />
     <div className="User-content">
-      <Usercard />
+      <Usercard user={{ id: '76561198141770676', url: "https://steamcommunity.com/id/yolozen/", name: "yolozen", avatar: "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/07/07449cfad239b6086174c9015f7cdc5627f6d4b1_full.jpg", armaHours: 2352}} />
     </div>
   </div>
 );
