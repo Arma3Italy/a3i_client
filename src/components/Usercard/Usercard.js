37374a25f69599f543f70ed961d9ee45758d449a
Kaj Oskar Rusilowski <xedom08@gmail.com>
2019-06-17 22:40:31 +0200

    usercard updated

--- src/components/Usercard/Usercard.js
+++ src/components/Usercard/Usercard.js
@@ -2,8 +2,13 @@ import React from 'react';
 import './Usercard.scss';
 
 const Usercard = (props) => {
+  const user = props.user;
 
-  return (<div className="Usercard"></div>);
+  return (<div className="Usercard">
+    <img src={user.avatar} alt="user avatar"/>
+    <h2><a href={user.url} target="_blank" rel="noopener noreferrer">{user.name}</a></h2>
+    <p>ARMA 3: {user.armaHours} ore</p>
+  </div>);
 };
 
 export default Usercard;
