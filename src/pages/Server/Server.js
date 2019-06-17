a043b1f6d41ae39fa8ab425ae21cbf29d108d3be
Kaj Oskar Rusilowski <xedom08@gmail.com>
2019-06-17 22:51:44 +0200

    created new variables

--- src/pages/Server/Server.js
+++ src/pages/Server/Server.js
@@ -5,23 +5,26 @@ import Table from './../../components/Table/Table.js';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const Servers = () => (
-  <div className="Servers">
+const Servers = () => {
+  
+  const serverlist = [
+    {id: 1, rank:5, name:<a href="#server1-link">appesolife</a>, players:82, modalita:"RPG", join:<a href="#server1" ><FontAwesomeIcon size="lg" icon={['fas', 'sign-in-alt']} /></a>},
+    {id: 2, rank:2, name:<a href="#server2-link">universallife</a>, players:18, modalita:"RPG", join:<a href="#server2" ><FontAwesomeIcon size="lg" icon={['fas', 'sign-in-alt']} /></a>},
+    {id: 3, rank:4, name:<a href="#server3-link">putinlife</a>, players:122, modalita:"RPG", join:<a href="#server3" ><FontAwesomeIcon size="lg" icon={['fas', 'sign-in-alt']} /></a>},
+    {id: 4, rank:1, name:<a href="#server4-link">lesslife</a>, players:5, modalita:"RPG", join:<a href="#server4" ><FontAwesomeIcon size="lg" icon={['fas', 'sign-in-alt']} /></a>},
+    {id: 5, rank:4, name:<a href="#server5-link">octa mrp</a>, players:9, modalita:"CTF", join:<a href="#server5" ><FontAwesomeIcon size="lg" icon={['fas', 'sign-in-alt']} /></a>},
+    {id: 6, rank:6, name:<a href="#server6-link">maldenlife nuova chiusura</a>, players:22, modalita:"RPG", join:<a href="#server6" ><FontAwesomeIcon size="lg" icon={['fas', 'sign-in-alt']} /></a>},
+    {id: 7, rank:3, name:<a href="#server7-link">tumplife</a>, players:14, modalita:"RPG", join:<a href="#server7" ><FontAwesomeIcon size="lg" icon={['fas', 'sign-in-alt']} /></a>},
+  ];
+
+  return (<div className="Servers">
     <div className="Servers-hover"></div>
     <Navbar />
     <div className="Servers-content">
       <h2 className="title">Server List</h2>
-      <Table header={["Rank", "Name", "Players", "Modalità", "Entra"]} body={[
-        {id: 1, rank:5, name:<a href="#server1-link">appesolife</a>, players:82, modalita:"RPG", join:<a href="#server1" ><FontAwesomeIcon size="lg" icon={['fas', 'sign-in-alt']} /></a>},
-        {id: 2, rank:2, name:<a href="#server2-link">universallife</a>, players:18, modalita:"RPG", join:<a href="#server2" ><FontAwesomeIcon size="lg" icon={['fas', 'sign-in-alt']} /></a>},
-        {id: 3, rank:4, name:<a href="#server3-link">putinlife</a>, players:122, modalita:"RPG", join:<a href="#server3" ><FontAwesomeIcon size="lg" icon={['fas', 'sign-in-alt']} /></a>},
-        {id: 4, rank:1, name:<a href="#server4-link">lesslife</a>, players:5, modalita:"RPG", join:<a href="#server4" ><FontAwesomeIcon size="lg" icon={['fas', 'sign-in-alt']} /></a>},
-        {id: 5, rank:4, name:<a href="#server5-link">octa mrp</a>, players:9, modalita:"CTF", join:<a href="#server5" ><FontAwesomeIcon size="lg" icon={['fas', 'sign-in-alt']} /></a>},
-        {id: 6, rank:6, name:<a href="#server6-link">maldenlife nuova chiusura</a>, players:22, modalita:"RPG", join:<a href="#server6" ><FontAwesomeIcon size="lg" icon={['fas', 'sign-in-alt']} /></a>},
-        {id: 7, rank:3, name:<a href="#server7-link">tumplife</a>, players:14, modalita:"RPG", join:<a href="#server7" ><FontAwesomeIcon size="lg" icon={['fas', 'sign-in-alt']} /></a>},
-      ]} />
+      <Table header={["Rank", "Name", "Players", "Modalità", "Entra"]} body={serverlist} />
     </div>
-  </div>
-);
+  </div>)
+};
 
 export default Servers;
--- src/pages/User/User.js
+++ src/pages/User/User.js
@@ -3,15 +3,26 @@ import './User.scss';
 import Navbar from '../../components/Navbar/Navbar.js';
 import Usercard from '../../components/Usercard/Usercard.js';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const User = () => (
-  <div className="User">
+const User = () => {
+
+  const serversowned = [
+    {id: 2, rank:2, name:<a href="#server2-link">universallife</a>, players:18, modalita:"RPG", join:<a href="#server2" ><FontAwesomeIcon size="lg" icon={['fas', 'sign-in-alt']} /></a>},
+    {id: 3, rank:4, name:<a href="#server3-link">putinlife</a>, players:122, modalita:"RPG", join:<a href="#server3" ><FontAwesomeIcon size="lg" icon={['fas', 'sign-in-alt']} /></a>},
+    {id: 6, rank:6, name:<a href="#server6-link">maldenlife nuova chiusura</a>, players:22, modalita:"RPG", join:<a href="#server6" ><FontAwesomeIcon size="lg" icon={['fas', 'sign-in-alt']} /></a>},
+  ];
+
+  const user = { id: '76561198141770676', url: "https://steamcommunity.com/id/yolozen/", name: "yolozen", avatar: "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/07/07449cfad239b6086174c9015f7cdc5627f6d4b1_full.jpg", armaHours: 2352};
+
+  return (<div className="User">
     <div className="User-hover"></div>
     <Navbar />
     <div className="User-content">
-      <Usercard user={{ id: '76561198141770676', url: "https://steamcommunity.com/id/yolozen/", name: "yolozen", avatar: "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/07/07449cfad239b6086174c9015f7cdc5627f6d4b1_full.jpg", armaHours: 2352}} />
+      <Usercard user={user} />
+      <div className="serversOwned">
+      </div>
     </div>
-  </div>
-);
-
+  </div>);
+};
 export default User;
