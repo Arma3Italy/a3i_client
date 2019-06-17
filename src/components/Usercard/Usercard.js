e82358b2faf36a532e196bbdcefd8eb48363ce5f
Kaj Oskar Rusilowski <xedom08@gmail.com>
2019-06-17 16:46:57 +0200

    added usercard component

---
+++ src/components/Usercard/Usercard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Usercard.scss';
+
+const Usercard = (props) => {
+
+  return (<div className="Usercard"></div>);
+};
+
+export default Usercard;
--- src/pages/User/User.js
+++ src/pages/User/User.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './User.scss';
 import Navbar from '../../components/Navbar/Navbar.js';
+import Usercard from '../../components/Usercard/Usercard.js';
+
 
 const User = () => (
   <div className="User">
     <div className="User-hover"></div>
     <Navbar />
-    <div className="User-content"></div>
+    <div className="User-content">
+      <Usercard />
+    </div>
   </div>
 );
 
