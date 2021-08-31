f203e4e321d031d5f3ce35b8c31c0b9a66e5e3d0
Kaj Oskar Rusilowski <Kaj.Oskar1999@hotmail.com>
2021-08-31 07:48:49 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
-// import * as serviceWorker from './serviceWorker';
 import { BrowserRouter, Route } from 'react-router-dom';
+import './index.scss';
+import reportWebVitals from './reportWebVitals';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
@@ -15,14 +15,19 @@ import UserPage from './pages/User/User';
 library.add(fas, fab)
 
 ReactDOM.render(
+  <React.StrictMode>
     <BrowserRouter>
         <React.Fragment>
             <Route exact path="/" component={HomePage} />
             <Route exact path="/server" component={ServerPage} />
             <Route exact path="/user/:id" component={UserPage} />
         </React.Fragment>
-    </BrowserRouter>,
-    document.getElementById('root')
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
-// serviceWorker.register();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
