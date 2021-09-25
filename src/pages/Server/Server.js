9f28fef000ca707b27913d4a73008e395ebf5ffc
Kaj Oskar Rusilowski <Kaj.Oskar1999@hotmail.com>
2021-09-25 21:29:57 +0200

    some fix

--- src/pages/Server/Server.js
+++ src/pages/Server/Server.js
@@ -8,13 +8,13 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 const Servers = () => {
   
   const serverlist = [
-    {id: 1, rank:5, name:<a href="#server1-link">appesolife</a>, players:82, modalita:"RPG", join:<a href="#server1" ><FontAwesomeIcon size="lg" icon={['fas', 'sign-in-alt']} /></a>},
-    {id: 2, rank:2, name:<a href="#server2-link">universallife</a>, players:18, modalita:"RPG", join:<a href="#server2" ><FontAwesomeIcon size="lg" icon={['fas', 'sign-in-alt']} /></a>},
-    {id: 3, rank:4, name:<a href="#server3-link">putinlife</a>, players:122, modalita:"RPG", join:<a href="#server3" ><FontAwesomeIcon size="lg" icon={['fas', 'sign-in-alt']} /></a>},
-    {id: 4, rank:1, name:<a href="#server4-link">lesslife</a>, players:5, modalita:"RPG", join:<a href="#server4" ><FontAwesomeIcon size="lg" icon={['fas', 'sign-in-alt']} /></a>},
-    {id: 5, rank:4, name:<a href="#server5-link">octa mrp</a>, players:9, modalita:"CTF", join:<a href="#server5" ><FontAwesomeIcon size="lg" icon={['fas', 'sign-in-alt']} /></a>},
-    {id: 6, rank:6, name:<a href="#server6-link">maldenlife nuova chiusura</a>, players:22, modalita:"RPG", join:<a href="#server6" ><FontAwesomeIcon size="lg" icon={['fas', 'sign-in-alt']} /></a>},
-    {id: 7, rank:3, name:<a href="#server7-link">tumplife</a>, players:14, modalita:"RPG", join:<a href="#server7" ><FontAwesomeIcon size="lg" icon={['fas', 'sign-in-alt']} /></a>},
+    {id: 1, rank:5, name:<a href="#server1-link">appesolife</a>, players:82, modalita:"RPG", join:<a href="#server1-join" ><FontAwesomeIcon size="lg" icon={['fas', 'sign-in-alt']} /></a>},
+    {id: 2, rank:2, name:<a href="#server2-link">universallife</a>, players:18, modalita:"RPG", join:<a href="#server2-join" ><FontAwesomeIcon size="lg" icon={['fas', 'sign-in-alt']} /></a>},
+    {id: 3, rank:4, name:<a href="#server3-link">putinlife</a>, players:122, modalita:"RPG", join:<a href="#server3-join" ><FontAwesomeIcon size="lg" icon={['fas', 'sign-in-alt']} /></a>},
+    {id: 4, rank:1, name:<a href="#server4-link">lesslife</a>, players:5, modalita:"RPG", join:<a href="#server4-join" ><FontAwesomeIcon size="lg" icon={['fas', 'sign-in-alt']} /></a>},
+    {id: 5, rank:4, name:<a href="#server5-link">octa mrp</a>, players:9, modalita:"CTF", join:<a href="#server5-join" ><FontAwesomeIcon size="lg" icon={['fas', 'sign-in-alt']} /></a>},
+    {id: 6, rank:6, name:<a href="#server6-link">maldenlife nuova chiusura</a>, players:22, modalita:"RPG", join:<a href="#server6-join" ><FontAwesomeIcon size="lg" icon={['fas', 'sign-in-alt']} /></a>},
+    {id: 7, rank:3, name:<a href="#server7-link">tumplife</a>, players:14, modalita:"RPG", join:<a href="#server7-join" ><FontAwesomeIcon size="lg" icon={['fas', 'sign-in-alt']} /></a>},
   ];
 
   return (<div className="Servers">
